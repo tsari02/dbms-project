2db02d8917acdc0d55f4bb8ce7a650e2cad1f23b
aditya-chhaparia <adityachhaparia101@gmail.com>
2022-10-31 16:16:47 +0530

    Change @RestController to @Controller

--- spring-boot-example/src/main/java/com/dbms/project/api/CustomerController.java
+++ spring-boot-example/src/main/java/com/dbms/project/api/CustomerController.java
@@ -3,12 +3,13 @@ package com.dbms.project.api;
 import com.dbms.project.model.Customer;
 import com.dbms.project.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequestMapping("api/customer")
-@RestController
+@Controller
 public class CustomerController {
     private final CustomerService customerService;
 
@@ -18,26 +19,31 @@ public class CustomerController {
     }
 
     @PostMapping
+    @ResponseBody
     public void addCustomer(@RequestBody Customer customer) {
         customerService.insertCustomer(customer);
     }
 
     @GetMapping
+    @ResponseBody
     public List<Customer> getAllCustomers() {
         return customerService.getAllCustomers();
     }
 
     @DeleteMapping(path="{id}")
+    @ResponseBody
     public void deleteCustomer(@PathVariable("id") int id) {
         customerService.deleteCustomer(id);
     }
 
     @GetMapping(path="{id}")
+    @ResponseBody
     public Customer getCustomerById(@PathVariable("id") int id) {
         return customerService.getCustomerById(id);
     }
 
     @PutMapping(path="{id}")
+    @ResponseBody
     public void updateCustomer(@PathVariable("id") int id, @RequestBody Customer customer) {
         customerService.updateCustomer(id, customer);
     }
