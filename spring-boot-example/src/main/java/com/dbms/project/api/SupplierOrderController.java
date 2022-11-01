c9f80da8cb123731eb695aa100bac7e737ec3b4d
aditya-chhaparia <adityachhaparia101@gmail.com>
2022-11-01 19:40:08 +0530

    Add java files for SupplierOrder

---
+++ spring-boot-example/src/main/java/com/dbms/project/api/SupplierOrderController.java
@@ -0,0 +1,50 @@
+package com.dbms.project.api;
+
+import com.dbms.project.model.SupplierOrder;
+import com.dbms.project.service.SupplierOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("api/supplierOrder")
+@Controller
+public class SupplierOrderController {
+    private final SupplierOrderService supplierOrderService;
+
+    @Autowired
+    public SupplierOrderController(SupplierOrderService supplierOrderService) {
+        this.supplierOrderService = supplierOrderService;
+    }
+
+    @PostMapping
+    @ResponseBody
+    public void addSupplierOrder(@RequestBody SupplierOrder supplierOrder) {
+        supplierOrderService.insertSupplierOrder(supplierOrder);
+    }
+
+    @GetMapping
+    @ResponseBody
+    public List<SupplierOrder> getAllSupplierOrders() {
+        return supplierOrderService.getAllSupplierOrders();
+    }
+
+    @DeleteMapping(path="{id}")
+    @ResponseBody
+    public void deleteSupplierOrder(@PathVariable("id") int id) {
+        supplierOrderService.deleteSupplierOrder(id);
+    }
+
+    @GetMapping(path="{id}")
+    @ResponseBody
+    public SupplierOrder getSupplierOrderById(@PathVariable("id") int id) {
+        return supplierOrderService.getSupplierOrderById(id);
+    }
+
+    @PutMapping(path="{id}")
+    @ResponseBody
+    public void updateSupplierOrder(@PathVariable("id") int id, @RequestBody SupplierOrder supplierOrder) {
+        supplierOrderService.updateSupplierOrder(id, supplierOrder);
+    }
+}
--- spring-boot-example/src/main/java/com/dbms/project/dao/SupplierOrderDao.java
+++ spring-boot-example/src/main/java/com/dbms/project/dao/SupplierOrderDao.java
@@ -1,5 +1,44 @@
 package com.dbms.project.dao;
 
-public class SupplierOrderDao {
+import com.dbms.project.model.SupplierOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public class SupplierOrderDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public SupplierOrderDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public int insertSupplierOrder(SupplierOrder supplierOrder) {
+        final String sql = "INSERT INTO supplierOrder(dateOfOrder, status, supplierId) VALUES(?, ?, ?)";
+        return jdbcTemplate.update(sql, supplierOrder.getDateOfOrder(), supplierOrder.getStatus(), supplierOrder.getSupplierId());
+    }
+
+    public List<SupplierOrder> getAllSupplierOrders() {
+        final String sql = "SELECT * from supplierOrder";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(SupplierOrder.class));
+    }
+
+    public SupplierOrder getSupplierOrderById(int id) {
+        final String sql = "SELECT * from supplierOrder WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[] {id}, new BeanPropertyRowMapper<>(SupplierOrder.class));
+    }
+
+    public int deleteSupplierOrder(int id) {
+        final String sql = "DELETE FROM supplierOrder WHERE id = ?";
+        return jdbcTemplate.update(sql, id);
+    }
+
+    public int updateSupplierOrder(int id, SupplierOrder supplierOrder) {
+        final String sql = "UPDATE supplierOrder SET dateOfOrder = ?, status = ?, supplierId = ? WHERE id = ?";
+        return jdbcTemplate.update(sql, supplierOrder.getDateOfOrder(), supplierOrder.getStatus(), supplierOrder.getSupplierId(), id);
+    }
 }
