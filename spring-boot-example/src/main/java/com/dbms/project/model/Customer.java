153c17f755592bf8effdf5bffe1d2ad9c669f2d6
aditya-chhaparia <adityachhaparia101@gmail.com>
2022-11-01 15:40:25 +0530

    Add validation for customer model

--- spring-boot-example/src/main/java/com/dbms/project/model/Customer.java
+++ spring-boot-example/src/main/java/com/dbms/project/model/Customer.java
@@ -2,20 +2,34 @@ package com.dbms.project.model;
 
 import lombok.Data;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
 public class Customer {
     private int id;
+    @NotBlank
     private String firstName;
     private String middleName;
     private String lastName;
+    @NotBlank
     private String contactNumber;
+    @NotNull
     private Date dateOfBirth;
+    @Email
+    @NotBlank
     private String emailId;
+    @NotBlank
     private String city;
+    @NotBlank
     private String state;
+    @NotBlank
     private String postalCode;
+    @NotBlank
     private String country;
+    @NotBlank
     private String street;
 }
