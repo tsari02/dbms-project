c5be9d4f27d4225ddacc8a3464737058c3898ff6
mannmj <manas.jain.cse20@itbhu.ac.in>
2022-11-07 02:01:44 +0530

    Add new supplier form

--- spring-boot-example/src/main/java/com/dbms/project/api/SupplierController.java
+++ spring-boot-example/src/main/java/com/dbms/project/api/SupplierController.java
@@ -1,11 +1,14 @@
 package com.dbms.project.api;
 
+import com.dbms.project.model.Customer;
 import com.dbms.project.model.Supplier;
 import com.dbms.project.service.SupplierService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -21,10 +24,14 @@ public class SupplierController {
         this.supplierService = supplierService;
     }
 
-    @PostMapping(path="/api/supplier")
-    @ResponseBody
-    public void addSupplier(@Valid @NotNull @RequestBody Supplier supplier) {
+    @PostMapping(path="/supplier")
+    public String addSupplier(@Valid Supplier supplier, BindingResult result, RedirectAttributes redirectAttributes, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("supplier", supplier);
+            return "supplier-new";
+        }
         supplierService.insertSupplier(supplier);
+        return "redirect:/supplier";
     }
 
     @GetMapping(path="/supplier")
@@ -49,5 +56,10 @@ public class SupplierController {
     @ResponseBody
     public void updateSupplier(@PathVariable("id") int id, @Valid @NotNull @RequestBody Supplier supplier) {
         supplierService.updateSupplier(id, supplier);
+    }
+    @GetMapping(path="/supplier/new")
+    public String addSupplierForm(Model model) {
+        model.addAttribute("supplier", new Supplier());
+        return "supplier-new";
     }
 }
