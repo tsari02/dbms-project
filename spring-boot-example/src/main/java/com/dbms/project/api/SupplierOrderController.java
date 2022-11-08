0df873c2a21be4ed42dc889a392aef07444b4a04
aditya-chhaparia <adityachhaparia101@gmail.com>
2022-11-08 22:37:38 +0530

    Fix getAllProductTypesInSupplierOrder

--- spring-boot-example/src/main/java/com/dbms/project/api/SupplierOrderController.java
+++ spring-boot-example/src/main/java/com/dbms/project/api/SupplierOrderController.java
@@ -1,6 +1,8 @@
 package com.dbms.project.api;
 
+import com.dbms.project.model.OrderedProductType;
 import com.dbms.project.model.SupplierOrder;
+import com.dbms.project.service.OrderedProductTypeService;
 import com.dbms.project.service.SupplierOrderService;
 import com.dbms.project.service.SupplierService;
 import com.dbms.project.service.ProductTypeService;
@@ -25,12 +27,14 @@ public class SupplierOrderController {
     private final SupplierOrderService supplierOrderService;
     private final SupplierService supplierService;
     private final ProductTypeService productTypeService;
+    private final OrderedProductTypeService orderedProductTypeService;
 
     @Autowired
-    public SupplierOrderController(SupplierOrderService supplierOrderService, SupplierService supplierService, ProductTypeService productTypeService) {
+    public SupplierOrderController(SupplierOrderService supplierOrderService, SupplierService supplierService, ProductTypeService productTypeService, OrderedProductTypeService orderedProductTypeService) {
         this.supplierOrderService = supplierOrderService;
         this.supplierService = supplierService;
         this.productTypeService = productTypeService;
+        this.orderedProductTypeService = orderedProductTypeService;
     }
 
     @PostMapping(path="/api/order/supplier")
@@ -83,7 +87,12 @@ public class SupplierOrderController {
 
     @PostMapping(path="/order/supplier/{id}/add")
     public String addProductsToSupplierOrder(@PathVariable("id") int supplierOrderId, @RequestParam("quantity") int quantity, @RequestParam("productTypeId") int productTypeId, Authentication authentication, RedirectAttributes redirectAttributes){
-        productTypeService.addProductTypeToSupplierOrder(productTypeId, quantity, supplierOrderId);
+        OrderedProductType orderedProductType = new OrderedProductType();
+        orderedProductType.setProductTypeId(productTypeId);
+        orderedProductType.setQuantity(quantity);
+        orderedProductType.setSupplierOrderId(supplierOrderId);
+        orderedProductType.setNumberDelivered(0);
+        orderedProductTypeService.insertOrderedProductType(orderedProductType);
         return "redirect:/order/supplier/" + supplierOrderId + "/add";
     }
 
