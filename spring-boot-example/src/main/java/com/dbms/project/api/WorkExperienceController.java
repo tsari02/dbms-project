f5baee6da4b664b82382318ecf6c0b357252e5ac
mankypanky <mayankpant2009@gmail.com>
2022-11-09 03:23:40 +0530

    Add workex

--- spring-boot-example/src/main/java/com/dbms/project/api/WorkExperienceController.java
+++ spring-boot-example/src/main/java/com/dbms/project/api/WorkExperienceController.java
@@ -4,7 +4,10 @@ import com.dbms.project.model.WorkExperience;
 import com.dbms.project.service.WorkExperienceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -51,5 +54,20 @@ public class WorkExperienceController {
     @ResponseBody
     public void updateWorkExperience(@PathVariable("id") int id, @Valid @NotNull @RequestBody WorkExperience workExperience) {
         workExperienceService.updateWorkExperience(id, workExperience);
+    }
+    @GetMapping(path="/employee/{id}/workExperience/add")
+    public String addWorkExperience(@PathVariable("id") int id, Model model) {
+        WorkExperience workExperience = new WorkExperience();
+        workExperience.setEmployeeId(id);
+        model.addAttribute("workExperience", workExperience);
+        return "add-workExperience";
+    }
+
+    @PostMapping(path="/employee/workexperience/add")
+    public String addWorkExperienceSubmit(@Valid @NotNull WorkExperience workExperience, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        // System.out.println(leavesAndSalaries.getEmployeeId());
+        // System.out.println(leave);
+        workExperienceService.insertWorkExperience(workExperience);
+        return "redirect:/employee/"+workExperience.getEmployeeId();
     }
 }
